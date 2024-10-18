00cdcfd25e9c0eca14b585c0e995b7c44779accf
Takwa Kochbati <kochbatitakwa1@gmail.com>
2024-10-18 18:59:54 +0200

    Pydantic

--- ice_breaker.py
+++ ice_breaker.py
@@ -7,31 +7,44 @@ from langchain_community.chat_models import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
+from langchain_community.llms import Ollama
 
+
+from output_parsers import summary_parser
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
 # from langchain.chains.summarize.refine_prompts import prompt_template
 
-def ice_breaker_with(name : str) -> str:
+
+def ice_breaker_with(name: str) -> str:
     linkedin_username = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url=linkedin_username, mock=True
+    )
     summary_template = """
        given the linkedin information {information} about a person, I want you to create:
        1. A short summary
        2. two interesting facts about them
+       \n{format_instructions}
        """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": summary_parser.get_format_instructions()
+        },
     )
 
     # llm = ChatOpenAI(temperature = 0, model_name = "gpt-3.5-turbo")
     llm = ChatOllama(model="llama3.1")
-    chain = summary_prompt_template | llm | StrOutputParser()
-    #linkedin_data = scrape_linkedin_profile(
+    #llm = Ollama(model="mistral:7b-instruct-v0.2-q6_K")
+    chain = summary_prompt_template | llm |summary_parser
+    #chain = summary_prompt_template | llm | StrOutputParser()
+    # linkedin_data = scrape_linkedin_profile(
     #    linkedin_profile_url="https://linkedin.com/in/eden-marco/", mock=True
-    #)
+    # )
     res = chain.invoke(input={"information": linkedin_data})
     print(res)
 
@@ -40,7 +53,6 @@ if __name__ == "__main__":
     load_dotenv()
     print("Ice breaker Enter")
     ice_breaker_with(name="Eden Marco")
-
 
     '''
     print(os.environ["COOL_API_KEY"])
--- third_parties/twitter.py
+++ third_parties/twitter.py
@@ -1,15 +1,17 @@
 import os
 from dotenv import load_dotenv
-#import tweepy
+
+# import tweepy
 import requests
 
 load_dotenv()
 
+
 def scrape_user_tweets(username, num_tweets=5, mock: bool = False):
     """
-       Scrapes a Twitter user's original tweets (i.e., not retweets or replies) and returns them as a list of dictionaries.
-       Each dictionary has three fields: "time_posted" (relative to now), "text", and "url".
-       """
+    Scrapes a Twitter user's original tweets (i.e., not retweets or replies) and returns them as a list of dictionaries.
+    Each dictionary has three fields: "time_posted" (relative to now), "text", and "url".
+    """
     tweet_list = []
     if mock:
         EDEN_TWITTER_GIST = "https://gist.githubusercontent.com/emarco177/827323bb599553d0f0e662da07b9ff68/raw/57bf38cf8acce0c87e060f9bb51f6ab72098fbd6/eden-marco-twitter.json"
@@ -22,6 +24,7 @@ def scrape_user_tweets(username, num_tweets=5, mock: bool = False):
         tweet_list.append(tweet_dict)
 
     return tweet_list
+
 
 if __name__ == "__main__":
 
